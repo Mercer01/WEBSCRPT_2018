cb570ccdfabdb990e1a405eba603f70e6bb8144c
Sam Mercer <sam@mercerfamily.org.uk>
2018-11-09 13:35:34 +0000

    completed ws_node

---
+++ ws_node/worksheet/utility.js
@@ -0,0 +1,18 @@
+function add(a,b) {
+    return a + b;
+}
+module.exports.add = add;
+  
+
+function compare(var1, var2) {
+	return (var1.toString() === var2.toString());
+}
+
+module.exports.compare = compare;
+
+function largest(large_array) {
+    let max = Math.max(...large_array);
+    return max;
+}
+module.exports.largest = largest;
+
---
+++ ws_node/worksheet/webserver.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const url = require('url');
+const util = require('./utility');
+
+const server = http.createServer(
+  (request, response) => {
+    const parsedUrl = url.parse(request.url, true);
+    if (parsedUrl.pathname === '/add') {
+        response.setHeader("Content-Type", "text/plain");
+        let sum = util.add(parseFloat(parsedUrl.query.a), parseFloat(parsedUrl.query.b));
+        response.end(sum.toString());
+    } else {
+      response.statusCode = 404;
+      response.end('Not found!\n');
+    }
+  }
+);
+server.listen(8080);
+// then go to http://localhost:8080/hello
+// and http://localhost:8080/foo
